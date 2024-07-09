1435fad3ce34f620b9033674b506506eb52982f3
hosseinmir0 <146254983+hosseinmir0@users.noreply.github.com>
2024-07-09 20:40:59 +0330

    logic count.py

---
+++ logic count.py
@@ -0,0 +1,30 @@
+# made by : hossein_mir0
+# telegram id : hossein_mir0
+
+vorodi = input('enter your text: ')
+list_khali = []
+
+for shomaresh in vorodi :
+    if shomaresh not in list_khali :
+        num = 0
+        list_khali.append(shomaresh)
+        for teadad in vorodi :
+            if teadad == shomaresh :
+                num += 1
+        
+        print (f'word is: {shomaresh} , counts is: {num}')
+        
+# filter repeated word
+
+vorodi = input('enter your text: ')
+list_khali = []
+
+for shomaresh in vorodi :
+    if shomaresh not in list_khali :
+        num = 0
+        list_khali.append(shomaresh)
+        for teadad in vorodi :
+            if teadad == shomaresh :
+                num += 1
+        if num >= 2 and teadad  :
+            print (f'word is: {shomaresh} , counts is: {num}')
